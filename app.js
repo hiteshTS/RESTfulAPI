c9738f7b71dfeeedef833360a59318621cf7e145
hiteshTS <137639073+hiteshTS@users.noreply.github.com>
2023-12-10 23:15:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,86 +2,93 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 3000;
+const port = 3000;
 
-// Middleware to parse JSON requests
 app.use(bodyParser.json());
 
 // In-memory storage for tasks
-let tasks = [
-  { id: 1, title: 'Ram', description: 'boy' },
-  { id: 2, title: 'sita', description: 'girl' },
-   { id: 3, title: 'lakshaman', description: 'Boy'},
-];
+let tasks = [];
+let taskIdCounter = 1;
 
-// Middleware for basic validation
-function validateTask(req, res, next) {
-  const { title, description } = req.body;
-  if (!title || !description) {
-    return res.status(400).json({ error: 'Title and description are required.' });
-  }
-  next();
-}
-
-// Routes
-
+// API endpoints
 app.get('/tasks', (req, res) => {
-  const page = req.query.page || 1;
-  const pageSize = 5;
-  const startIndex = (page - 1) * pageSize;
-  const endIndex = startIndex + pageSize;
-  const paginatedTasks = tasks.slice(startIndex, endIndex);
+    // Bonus: Implement pagination
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = parseInt(req.query.pageSize) || 10;
+    const startIndex = (page - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+    const paginatedTasks = tasks.slice(startIndex, endIndex);
 
-  res.json(paginatedTasks);
+    res.status(200).json(paginatedTasks);
 });
-
 
 app.get('/tasks/:id', (req, res) => {
-  const taskId = parseInt(req.params.id);
-  const task = tasks.find(task => task.id === taskId);
+    const taskId = parseInt(req.params.id);
+    const task = tasks.find(t => t.id === taskId);
 
-  if (!task) {
-    return res.status(404).json({ error: 'Task not found' });
-  }
-
-  res.json(task);
+    if (task) {
+        res.status(200).json(task);
+    } else {
+        res.status(404).json({ error: 'Task not found' });
+    }
 });
 
+app.post('/tasks', (req, res) => {
+    const { title, description } = req.body;
 
-app.post('/tasks', validateTask, (req, res) => {
-  const { title, description } = req.body;
-  const newTask = { id: tasks.length + 1, title, description };
-  tasks.push(newTask);
-  res.status(201).json(newTask);
+    // Basic validation: Ensure title and description are present
+    if (!title || !description) {
+        res.status(400).json({ error: 'Title and description are required' });
+    } else {
+        const newTask = {
+            id: taskIdCounter++,
+            title,
+            description,
+        };
+        tasks.push(newTask);
+        res.status(201).json(newTask);
+    }
 });
 
+app.put('/tasks/:id', (req, res) => {
+    const taskId = parseInt(req.params.id);
+    const taskIndex = tasks.findIndex(t => t.id === taskId);
 
-app.put('/tasks/:id', validateTask, (req, res) => {
-  const taskId = parseInt(req.params.id);
-  const updatedTaskIndex = tasks.findIndex(task => task.id === taskId);
-
-  if (updatedTaskIndex === -1) {
-    return res.status(404).json({ error: 'Task not found' });
-  }
-
-  const { title, description } = req.body;
-  tasks[updatedTaskIndex] = { ...tasks[updatedTaskIndex], title, description };
-  res.json(tasks[updatedTaskIndex]);
+    if (taskIndex !== -1) {
+        const { title, description } = req.body;
+        tasks[taskIndex] = { id: taskId, title, description };
+        res.status(200).json(tasks[taskIndex]);
+    } else {
+        res.status(404).json({ error: 'Task not found' });
+    }
 });
 
 app.delete('/tasks/:id', (req, res) => {
-  const taskId = parseInt(req.params.id);
-  tasks = tasks.filter(task => task.id !== taskId);
-  res.sendStatus(204);
+    const taskId = parseInt(req.params.id);
+    tasks = tasks.filter(t => t.id !== taskId);
+    res.status(204).send();
+});
+app.get('/tasks', (req, res) => {
+    let filteredTasks = [...tasks];
+
+    // Sorting
+    const sortBy = req.query.sortBy;
+    if (sortBy) {
+        filteredTasks.sort((a, b) => a[sortBy].localeCompare(b[sortBy]));
+    }
+
+    // Filtering
+    const filter = req.query.filter;
+    if (filter) {
+        filteredTasks = filteredTasks.filter(task =>
+            task.title.toLowerCase().includes(filter.toLowerCase()) ||
+            task.description.toLowerCase().includes(filter.toLowerCase())
+        );
+    }
+
+    res.status(200).json(filteredTasks);
 });
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: 'Internal Server Error' });
-});
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
